25504b1c7c7739c563d2ad35d4c56cb2c79683da
sonirn <sonirn420@gmail.com>
2025-03-12 11:10:18 +0530

    Update history.js

--- history.js
+++ history.js
@@ -13,18 +13,6 @@ function loadHistory() {
     let history = JSON.parse(localStorage.getItem("imageHistory")) || [];
 
     history.forEach(item => {
-        const historyItem = document.createElement("div");
-        historyItem.classList.add("history-item");
-
-        const img = document.createElement("img");
-        img.src = item.imgUrl;
-        img.alt = item.prompt;
-
-        const caption = document.createElement("p");
-        caption.textContent = item.prompt;
-
-        historyItem.appendChild(img);
-        historyItem.appendChild(caption);
-        historyContainer.appendChild(historyItem);
+        displayImage(item.prompt, item.imgUrl, historyContainer);
     });
 }
